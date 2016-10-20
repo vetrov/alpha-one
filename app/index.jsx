80e38bf8e4125ca41d62c639d4c9eeb93b406b75
Aleksandr Vetrov <aleksandr.vetrov@frosmo.com>
2016-10-20 23:13:29 +0300

    Divide code to components plus notes deletion

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-if(process.env.NODE_ENV !== 'production') {
-  React.Perf = require('react-addons-perf');
+import App from './components/App';
+
+if (process.env.NODE_ENV !== 'production') {
+    React.Perf = require('react-addons-perf');
 }
 
 ReactDOM.render(
-  <div>Hello world</div>,
-  document.getElementById('app')
+    <App />,
+    document.getElementById('app')
 );
