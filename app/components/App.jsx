8c8c3a4331012fbf33820fc32f6d410a007886e9
Aleksandr Vetrov <aleksandr.vetrov@frosmo.com>
2016-10-23 23:52:00 +0300

    Notes are now editable

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -20,7 +20,12 @@ export default class App extends React.Component {
         return (
             <div>
                 <button onClick={this.addNote}>+</button>
-                <Notes notes={notes} onDelete={this.deleteNote}/>
+                <Notes
+                  notes={notes}
+                  onNoteClick={this.activateNoteEdit}
+                  onEdit={this.editNote}
+                  onDelete={this.deleteNote}
+                  />
             </div>
         );
     }
@@ -48,5 +53,28 @@ export default class App extends React.Component {
         this.setState({
             notes: this.state.notes.filter(note => note.id !== id)
         })
+    }
+
+    activateNoteEdit = (id) => {
+        this.setState({
+            notes: this.state.notes.map(note => {
+                if (note.id === id) {
+                    note.editing = true;
+                }
+                return note;
+            })
+        });
+    }
+
+    editNote = (id, task) => {
+        this.setState({
+            notes: this.state.notes.map(note => {
+                if (note.id === id) {
+                    note.editing = false;
+                    note.task = task;
+                }
+                return note;
+            })
+        });
     }
 }
