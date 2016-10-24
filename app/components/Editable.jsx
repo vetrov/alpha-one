18f0f6a77cded35a39b2ae9beb2d37fc7ff34ea1
Aleksandr Vetrov <aleksandr.vetrov@frosmo.com>
2016-10-24 23:28:54 +0300

    Some styling

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import classnames from 'classnames';
 
-export default ({editing, value, onEdit, ...props}) => {
+export default ({editing, value, onEdit, className, ...props}) => {
     if (editing) {
-        return <Edit value={value} onEdit={onEdit} {...props} />;
+        return <Edit
+            className={className}
+            value={value}
+            onEdit={onEdit}
+            {...props} />;
     }
 
-    return <span {...props}>{value}</span>;
+    return <span className={classnames('value', className)} {...props}>{value}</span>;
 }
 
 class Edit extends React.Component {
     render() {
-        const {value, ...props} = this.props;
+        const {className, value, ...props} = this.props;
 
         return <input
             type="text"
